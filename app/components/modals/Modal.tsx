97e7f561e7e39058b940b95766b13e7dbdd714b6
Nataliia Tsirul <tcirulnatalya@gmail.com>
2024-02-26 13:45:44 +0000

    added updates with Google and Github buttons

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -198,6 +198,7 @@ const Modal: React.FC<ModalProps> = ({
                     onClick={handleSubmit}
                   />
                 </div>
+                {footer}
               </div>
             </div>
           </div>
--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -11,6 +11,8 @@ import Modal from './Modal';
 import Heading from '../Heading';
 import Input from '../inputs/Input';
 import toast from 'react-hot-toast';
+import { defaultOverrides } from 'next/dist/server/require-hook';
+import Button from '../Button';
 
 const RegisterModal = () => {
   const registerModal = useRegisterModal();
@@ -42,6 +44,7 @@ const RegisterModal = () => {
         setIsLoading(false);
       });
   };
+
   const bodyContent = (
     <div className="flex flex-col gap-4">
       <Heading title="Welcome to AirBnB " subtitle="Create an Account" />
@@ -73,6 +76,51 @@ const RegisterModal = () => {
     </div>
   );
 
+  const footerContent = (
+    <div
+      className="
+  flex flex-col gap-4 mt-3"
+    >
+      <hr />
+      <Button
+        outline
+        label="Continue with Google"
+        icon={FcGoogle}
+        onClick={() => {}}
+      />
+      <Button
+        outline
+        label="Continue with GitHub"
+        icon={AiFillGithub}
+        onClick={() => {}}
+      />
+      <div
+        className="
+      text-neutral-500 text-center mt-4 font-light"
+      >
+        <div
+          className=" 
+         justify-center 
+         flex
+         flex-row
+         items-center 
+         gap-2"
+        >
+          <div>Already have an account?</div>
+        </div>
+        <div onClick={registerModal.onClose}
+          className="
+          text-neutral-800
+          cursor-pointer
+          hover:underline
+        "
+        >
+          <div>Log in</div>
+        </div>
+      </div>
+    </div>
+  );
+
   return (
     <Modal
       disabled={isLoading}
@@ -82,6 +130,7 @@ const RegisterModal = () => {
       onClose={registerModal.onClose}
       onSubmit={handleSubmit(onSubmit)}
       body={bodyContent}
+      footer={footerContent}
     />
   );
 };
