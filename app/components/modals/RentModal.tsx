1e895f9285b8dd59a25679ea715ee18689b0ce9a
Nataliia Tsirul <tcirulnatalya@gmail.com>
2024-03-09 16:09:18 +0000

    added selection of the countries

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -6,6 +6,7 @@ import Heading from '../Heading';
 import { categories } from '../navbar/Categories';
 import CategoryInput from '../inputs/CategoryInput';
 import { FieldValues, useForm } from 'react-hook-form';
+import CountrySelect from '../inputs/CountrySelect';
 enum STEPS {
   CATEGORY = 0,
   LOCATION = 1,
@@ -112,11 +113,31 @@ const RentModal = () => {
       </div>
     </div>
   );
+
+  if (step === STEPS.LOCATION) {
+    bodyContent = (
+      <div
+        className="
+      fle
+      
+      fle-col
+      gap-8
+      "
+      >
+        <Heading
+          title="Where ois your place located?"
+          subtitle="Help guests find you!"
+        />
+        <CountrySelect />
+      </div>
+    );
+  }
+
   return (
     <Modal
       isOpen={rentModal.isOpen}
       onClose={rentModal.onClose}
-      onSubmit={rentModal.onClose}
+      onSubmit={onNext}
       actionLabel={actionLabel}
       secondaryActionLabel={secondaryActionLabel}
       secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
