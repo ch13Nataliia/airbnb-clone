b0864aea8afc71c9917ddb0e5265d78b558893c9
Nataliia Tsirul <tcirulnatalya@gmail.com>
2024-03-11 08:53:53 +0000

    installed clodinary

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -10,6 +10,7 @@ import { FieldValues, useForm } from 'react-hook-form';
 import CountrySelect from '../inputs/CountrySelect';
 import dynamic from 'next/dynamic';
 import Counter from '../inputs/Counter';
+import ImageUpload from '../inputs/ImageUpload';
 
 enum STEPS {
   CATEGORY = 0,
@@ -185,6 +186,21 @@ const RentModal = () => {
           value={bathroomCount}
           onChange={(value) => setCustomValue('bathroomCount', value)}
         />
+      </div>
+    );
+  }
+
+  if (step === STEPS.IMAGES) {
+    bodyContent = (
+      <div
+        className="
+      flex
+      flex-col
+      gap-8"
+      >
+        <Heading title="Add a photo of your place"
+        subtitle="Show guests what your place looks like!" />
+        <ImageUpload />
       </div>
     );
   }
